8a9ceab3a1c58c5f568e9a1787e68f9f44d8cc90
Alessandro Tonin <58313635+Lychfindel@users.noreply.github.com>
2023-09-16 23:59:24 +0200

    Fix install problem of Alac

--- install-shairport.sh
+++ install-shairport.sh
@@ -2,7 +2,7 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-: "${SHAIRPORT_VERSION:=4.2}"
+: "${SHAIRPORT_VERSION:=4.3}"
 
 echo
 echo -n "Do you want to install Shairport Sync AirPlay 2 Audio Receiver (shairport-sync v${SHAIRPORT_VERSION})? [y/N] "
@@ -12,6 +12,8 @@ if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 apt install --no-install-recommends build-essential git autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd libpulse-dev
 
 # ALAC
+CURR_FOLDER=$(pwd)
+cd /tmp
 git clone --depth 1 https://github.com/mikebrady/alac.git
 cd alac
 autoreconf -fi
@@ -21,6 +23,7 @@ make install
 ldconfig
 cd ..
 rm -rf alac
+cd $CURR_FOLDER
 
 # NQPTP
 git clone https://github.com/mikebrady/nqptp.git
@@ -36,7 +39,7 @@ rm -rf nqptp
 # Shairport Sync
 git clone https://github.com/mikebrady/shairport-sync.git
 cd shairport-sync
-git checkout 4.2
+git checkout ${SHAIRPORT_VERSION}
 autoreconf -fi
 ./configure --sysconfdir=/etc --with-alsa --with-pa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2 --with-apple-alac
 make -j $(nproc)
